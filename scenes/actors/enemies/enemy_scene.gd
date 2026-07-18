969359a3852aae9795d3a2d3fbe90db920e2c557
pjc8513-hub <pjc8513@gmail.com>
2026-07-18 15:52:16 -0400

    combat animatin prep 1

--- scenes/actors/enemies/enemy_scene.gd
+++ scenes/actors/enemies/enemy_scene.gd
@@ -1,8 +1,9 @@
 extends Node3D
 @onready var enemy_sprite: Sprite3D = $EnemySprite
-@onready var progress_bar: ProgressBar = $Node/CanvasLayer/ProgressBar
-@onready var hit_animation: ColorRect = $Node/CanvasLayer/HitAnimation
 @onready var damage_label: Label = $Node/CanvasLayer/HitAnimation/DamageLabel
+@onready var hit_animation: Sprite2D = $Node/CanvasLayer/HitAnimation
+@onready var health_bar: ProgressBar = $Node/CanvasLayer/HealthBar
+@onready var ui_anchor: Marker3D = $UIAnchor
 
 
 # Called when the node enters the scene tree for the first time.
