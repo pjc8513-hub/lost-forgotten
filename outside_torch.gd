35054f9a7fd159c7d5d10d5fd472f598ed5f8ec5
pjc8513-hub <pjc8513@gmail.com>
2026-07-26 17:38:16 -0400

    day night cycle 2

--- outside_torch.gd
+++ outside_torch.gd
@@ -1,8 +1,28 @@
 extends Node3D
+
+const LIGHTS_ON_TIME_SECONDS := 17 * 60 * 60
+const LIGHTS_OFF_TIME_SECONDS := 6 * 60 * 60
+
 @onready var omni_light_3d: OmniLight3D = $OmniLight3D
 @onready var mesh_instance_3d_3: MeshInstance3D = $MeshInstance3D3
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	WorldManager.dungeon_time_changed.connect(_on_dungeon_time_changed)
+	_on_dungeon_time_changed(WorldManager.dungeon_elapsed_time)
+
+
+func _exit_tree() -> void:
+	if WorldManager.dungeon_time_changed.is_connected(_on_dungeon_time_changed):
+		WorldManager.dungeon_time_changed.disconnect(_on_dungeon_time_changed)
+
+
+func _on_dungeon_time_changed(_elapsed_seconds: int) -> void:
+	var time_of_day := WorldManager.get_time_of_day_seconds() % WorldManager.SECONDS_PER_DAY
+	var should_be_lit := time_of_day >= LIGHTS_ON_TIME_SECONDS or time_of_day < LIGHTS_OFF_TIME_SECONDS
+	_set_lit(should_be_lit)
+
+
+func _set_lit(lit: bool) -> void:
+	omni_light_3d.visible = lit
+	mesh_instance_3d_3.visible = lit
