c8a0ab906853727b98ce3dd40c1da7bb2fb7fd47
pjc8513-hub <pjc8513@gmail.com>
2026-07-28 20:00:26 -0400

    melody set up

---
+++ resources/MelodyData.gd
@@ -0,0 +1,5 @@
+class_name MelodyData
+extends Resource
+
+@export var melodyID: StringName
+@export var notes: String
---
+++ resources/OrganData.gd
@@ -0,0 +1,5 @@
+class_name OrganData
+extends Node
+
+
+@export var organID: StringName
